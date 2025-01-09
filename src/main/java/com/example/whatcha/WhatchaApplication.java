70b19251d9a46bf4846bf38cb956c2d641614e04
N-jongsik <jongsik0625@naver.com>
2025-01-09 19:35:31 +0900

    first commit

---
+++ src/main/java/com/example/whatcha/WhatchaApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whatcha/WhatchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
