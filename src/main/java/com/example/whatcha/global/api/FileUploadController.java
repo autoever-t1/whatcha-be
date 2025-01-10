f8f82109f63a1c71e0e090e6695524411b72dd15
N-jongsik <jongsik0625@naver.com>
2025-01-10 11:41:56 +0900

    수정

--- src/main/java/com/example/whatcha/global/api/FileUploadController.java
+++ src/main/java/com/example/whatcha/global/api/FileUploadController.java
@@ -1,13 +1,32 @@
 package com.example.whatcha.global.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.whatcha.global.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RequestMapping("/upload")
 @RestController
 public class FileUploadController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "hello world!!!";
+    private final FileUploadService fileUploadService;
+
+    @PostMapping()
+    public ResponseEntity<String> uploadFile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+
+        long fileSize = multipartFile.getSize();
+
+        String uploadedFileUrl = fileUploadService.upload(multipartFile.getInputStream(), multipartFile.getOriginalFilename(), fileSize);
+
+        return new ResponseEntity<>(uploadedFileUrl, HttpStatus.CREATED);
     }
 }
+
