d65a4cec9c9e474eacaba91c5d476f7d425ff740
N-jongsik <jongsik0625@naver.com>
2025-01-13 01:51:04 +0900

    feat: AdminController Setting

--- src/main/java/com/example/whatcha/domain/admin/api/AdminController.java
+++ src/main/java/com/example/whatcha/domain/admin/api/AdminController.java
@@ -1,4 +1,24 @@
 package com.example.whatcha.domain.admin.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping("/order/stats/model")
+    public ResponseEntity<?> getStatsByModel() {
+        return null;
+    }
+
+    @GetMapping("/order/stats/age-groups")
+    public ResponseEntity<?> getStatsByAge() {
+        return null;
+    }
 }
+
