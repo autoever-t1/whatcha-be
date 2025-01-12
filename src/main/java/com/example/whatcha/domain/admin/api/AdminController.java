0696a1117860abd94c4c5a05e4ce6636e732de2e
N-jongsik <jongsik0625@naver.com>
2025-01-13 01:06:35 +0900

    admin & order setting

---
+++ src/main/java/com/example/whatcha/domain/admin/api/AdminController.java
@@ -0,0 +1,4 @@
+package com.example.whatcha.domain.admin.api;
+
+public class AdminController {
+}
---
+++ src/main/java/com/example/whatcha/domain/order/api/OrderController.java
@@ -0,0 +1,4 @@
+package com.example.whatcha.domain.order.api;
+
+public class OrderController {
+}
