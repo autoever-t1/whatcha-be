2921c549a89385e09cb30cc6d46509d38ac1b2c7
N-jongsik <jongsik0625@naver.com>
2025-01-16 16:51:38 +0900

    fix: web driver setting

--- src/main/java/com/example/whatcha/global/api/WebDriverController.java
+++ src/main/java/com/example/whatcha/global/api/WebDriverController.java
@@ -1,23 +1,23 @@
-package com.example.whatcha.global.api;
-
-import com.example.whatcha.global.service.WebDriverService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequiredArgsConstructor
-@RequestMapping("/crawling")
-@RestController
-public class WebDriverController {
-
-    private final WebDriverService webDriverService;
-
-    @GetMapping()
-    public String startCrawling() {
-            webDriverService.init(); // 드라이버 초기화
-            webDriverService.getCarListAndDetails(); // 크롤링 시작
-            webDriverService.close(); // 드라이버 종료
-            return "크롤링 성공";
-    }
-}
+//package com.example.whatcha.global.api;
+//
+//import com.example.whatcha.global.service.WebDriverService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequiredArgsConstructor
+//@RequestMapping("/crawling")
+//@RestController
+//public class WebDriverController {
+//
+//    private final WebDriverService webDriverService;
+//
+//    @GetMapping()
+//    public String startCrawling() {
+//            webDriverService.init(); // 드라이버 초기화
+//            webDriverService.getCarListAndDetails(); // 크롤링 시작
+//            webDriverService.close(); // 드라이버 종료
+//            return "크롤링 성공";
+//    }
+//}
