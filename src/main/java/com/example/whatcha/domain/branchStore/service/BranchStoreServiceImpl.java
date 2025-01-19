5eb46e8aefa0886e98da55ec7440d05b559ebb36
joo9837 <joo9837@naver.com>
2025-01-19 11:46:09 +0900

    feat(BranchStore): 지점 정보 api 구현

--- src/main/java/com/example/whatcha/domain/branchStore/service/BranchStoreServiceImpl.java
+++ src/main/java/com/example/whatcha/domain/branchStore/service/BranchStoreServiceImpl.java
@@ -2,8 +2,8 @@ package com.example.whatcha.domain.branchStore.service;
 
 import com.example.whatcha.domain.branchStore.dao.BranchStoreRepository;
 import com.example.whatcha.domain.branchStore.domain.BranchStore;
+import com.example.whatcha.domain.branchStore.dto.BranchStoreResDto;
 import com.example.whatcha.domain.branchStore.exception.BranchStoreNotFoundException;
-import com.example.whatcha.domain.usedCar.dto.response.BranchStoreResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,7 @@ import static com.example.whatcha.domain.branchStore.constant.BranchStoreExcepti
 @RequiredArgsConstructor
 public class BranchStoreServiceImpl implements BranchStoreService {
     private final BranchStoreRepository branchStoreRepository;
+
     @Override
     public BranchStoreResDto findBranchStore(Long branchStoreId) {
         BranchStore branchStore = branchStoreRepository.findById(branchStoreId)
