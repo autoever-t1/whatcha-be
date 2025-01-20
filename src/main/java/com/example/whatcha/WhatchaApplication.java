c7a7d6aa13ca7a7b31343b9762e8c1a3931957a0
junsoo1003 <royalex@dgu.ac.kr>
2025-01-20 14:26:48 +0900

    feat: 매일 자정 만료된 알림 자동 삭제 기능

--- src/main/java/com/example/whatcha/WhatchaApplication.java
+++ src/main/java/com/example/whatcha/WhatchaApplication.java
@@ -2,8 +2,10 @@ package com.example.whatcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.example.whatcha")
+@EnableScheduling
 public class WhatchaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/whatcha/domain/interest/dao/UserCarAlertRepository.java
+++ src/main/java/com/example/whatcha/domain/interest/dao/UserCarAlertRepository.java
@@ -3,7 +3,10 @@ package com.example.whatcha.domain.interest.dao;
 import com.example.whatcha.domain.interest.domain.UserCarAlert;
 import com.example.whatcha.domain.usedCar.domain.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +15,9 @@ public interface UserCarAlertRepository extends JpaRepository<UserCarAlert, Long
     List<UserCarAlert> findAllByUserId(Long userId);
     void deleteByUserIdAndModel_ModelId(Long userId, Long modelId);
     Optional<UserCarAlert> findByUserIdAndModel(Long userId, Model model);
+
+    @Modifying
+    @Query("DELETE FROM UserCarAlert u WHERE u.alertExpirationDate < :date")
+    int deleteExpiredAlerts(LocalDate date);
+
 }
