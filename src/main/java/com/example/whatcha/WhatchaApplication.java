bad98fe29dd7f0126ceb814177f908bc385fe1cf
N-jongsik <jongsik0625@naver.com>
2025-01-20 17:31:12 +0900

    fix: 관리자 매물등록 수정2

--- src/main/java/com/example/whatcha/WhatchaApplication.java
+++ src/main/java/com/example/whatcha/WhatchaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = "com.example.whatcha")
+@SpringBootApplication
 public class WhatchaApplication {
 
     public static void main(String[] args) {
